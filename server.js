1a8323600a291cc4abbefc928954b3177d56181b
Rashmika Sharma <rashmikasharma913@gmail.com>
2026-03-06 23:11:10 +0530

    Update port configuration for render

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.get("/", (req,res)=>{
   res.send("Server is working");
 });
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
